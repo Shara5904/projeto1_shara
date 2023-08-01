14b69f464bdbddda5bce77c1489f19b97ecddfc1
BrayanHubas <118532389+miuka22@users.noreply.github.com>
2023-08-01 16:31:03 -0300

    implementação da rota

--- src/cats/cats.controller.js
+++ src/cats/cats.controller.js
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import { Bind, Controller, Get, HTTPStatus, Param, Res} from '@nestjs/common';
 
+const GATO= [ 
+{
+    id:1,
+    name:'franeudos',
+    corDosOlhos:'preto',
+    raca:'sphynx'
+},
+{
+    id:2,
+    name:'curovo',
+    corDosOlhos:'cinza',
+    raca:'siamês'
+},
+{
+    id:3,
+    name:'franjinha',
+    corDosOlhos:'cinza',
+    raca:'siamês'
+}
+
+];
 @Controller('cats')
-export class CatsController {}
+export class CatsController {
+
+    @Get()
+    findALL() {
+        return GATO;
+    }
+    
+    @Get(':id')
+    @Bind(Param(), Res())
+    findOne(params) {
+        const gatoEncontrados = GATO.find(gato => gato.id === params.id);
+        if (gatoEncontrados){
+            res.HTTPStatus(HTTPStatus.ok).json(agtoEncontrado);
+        } else{
+            res.status(HTTPStatus.NOT_FOUND).send();
+        }
+
+    }
+}
