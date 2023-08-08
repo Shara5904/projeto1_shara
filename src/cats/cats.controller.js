fb2b920de973014b397aaeb84e56a250c5f992fe
shara5904 <srss1@aluno.ifal.edu.br>
2023-08-08 14:32:54 -0300

    adiciona implementação rotya delete

--- src/cats/cats.controller.js
+++ src/cats/cats.controller.js
@@ -42,8 +42,15 @@ export class CatsController {
     }
 
     @Delete(':id')
-    @Bind(Param('id'))
-    remove(id){
+    @Bind(Param('id'), Res())
+    remove(id, res){
+        const indexgatoEncontrados = GATO.findIndex(gato => gato.id ==id);
+        if(indexgatoEncontrados >= 0){
+            GATO.splice(indexgatoEncontrados, 1);
+            res.status(HttpStatus.NO_CONTENT).send();
+        }else{
+            res.status(HttpStatus.NOT_FOUND).send();
+        }
         return `removendo gato com id = ${id}.`;
     }
-}
+};
